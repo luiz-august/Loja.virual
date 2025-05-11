29ad00102e4b0c6a88b00a87df7b3c93d67773b9
luiz-august <luizaugustofranca4@gmail.com>
2025-05-10 22:02:45 -0300

    Add files via upload

--- erro.js
+++ erro.js
@@ -1,11 +1,17 @@
 window.addEventListener('offline', () => {
  setTimeout(() => {
-  window.location.href = 'error.html';
- }, 500); // Pequeno atraso para garantir a detecção
+  if (navigator.onLine === false) {
+   // Confirma que ainda está offline
+   window.location.href = 'error.html'; // Redireciona para a página de erro
+  }
+ }, 500);
 });
 
 window.addEventListener('online', () => {
  setTimeout(() => {
-  window.location.href = 'index.html';
+  if (navigator.onLine === true) {
+   // Confirma que está online novamente
+   window.location.href = 'index.html'; // Redireciona para a página inicial
+  }
  }, 500);
 });
